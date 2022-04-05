6dddb78dee86f8a6f4a3fca15c0be48906d28705
Björn Ekelund <bjorn@ekelund.nu>
2022-04-05 21:37:01 +0200

    Update

--- DXLCusForm1/Properties/AssemblyInfo.cs
+++ DXLCusForm1/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DXLCusForm1")]
+[assembly: AssemblyTitle("DXLogIcomControl")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DXLCusForm1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("DXLogIcomControl")]
+[assembly: AssemblyCopyright("Copyright Björn Ekelund SM7IUN © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
